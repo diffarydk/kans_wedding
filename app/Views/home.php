baaed3023825aeb578c3b68d6a200eccc368134f
diffarydk <diffarydzikrik23@gmail.com>
2023-10-05 08:51:24 +0700

    landing page

---
+++ app/Views/home.php
@@ -0,0 +1,154 @@
+   <div class="landing-page">
+       <div class="section-1">
+           <nav class="frame">
+               <img class="kwedding" src="resource/img/kanswedding.png" />
+               <div class="nav-frame">
+                   <a href="#" class="text-wrapper">Home</a>
+                   <a href="#" class="text-wrapper">Paket</a>
+                   <a href="#" class="text-wrapper">Produk</a>
+                   <div class="buat-sekarang-wrapper">
+                       <a href="#" class="buat-sekarang">Daftar/Login</a>
+                   </div>
+               </div>
+           </nav>
+           <div class="frame-home">
+               <div class="platform-undangan">Platform Undangan <br />Pernikahan Digital</div>
+               <p class="undangan-pernikahan">
+                   <span class="text-wrapper-home">Undangan pernikahan online dengan website </span>
+                   <span class="span">eksklusif</span>
+                   <span class="text-wrapper-home">. <br />Mulai buat undanganmu bersama kami dengan puluhan tema
+                       yang tersedia.</span>
+               </p>
+           </div>
+           <img class="wedding-invitation" src="resource/img/wedding-invitation.png" />
+           <div class="div-wrapper">
+               <a href="#" class="text-button-wrapper">Daftar Sekarang</a>
+           </div>
+       </div>
+       <div class="section-2">
+           <div class="heading-section-2">Undangan Pernikahan Digital</div>
+           <p class="sub-heading-section-2">
+               Undangan Online Digital Pernikahan, Aqiqah, Khitanan, Wisuda, dan Ulang Tahun. Murah, praktis &amp;
+               tanpa
+               antri. Hanya 10 menit, undangan digital siap dibagikan.
+           </p>
+           <div class="box-4-section-2">
+               <img class="vector" src="resource/img/pocket.svg" />
+               <div class="heading-box">Undangan Digital</div>
+               <p class="sub-heading-box">
+                   Kami menyediakan berbagai pilihan template untuk <br />undangan digital kamu. <br />Eits, ada
+                   ragam
+                   pilihan music juga loh
+               </p>
+           </div>
+           <div class="box-1-section-2">
+               <img class="vector" src="resource/img/scan.svg" />
+               <div class="heading-box">Scan QR Code</div>
+               <p class="sub-heading-box">
+                   Kami menyediakan berbagai pilihan template untuk <br />undangan digital kamu. <br />Eits, ada
+                   ragam
+                   pilihan music juga loh
+               </p>
+           </div>
+           <div class="box-2-section-2">
+               <img class="vector" src="resource/img/vcvid.svg" />
+               <div class="heading-box">Undangan Video</div>
+               <p class="sub-heading-box">
+                   Kami menyediakan berbagai pilihan template untuk <br />undangan digital kamu. <br />Eits, ada
+                   ragam
+                   pilihan music juga loh
+               </p>
+           </div>
+           <div class="box-3-section-2">
+               <img class="vector" src="resource/img/quick.svg" />
+               <div class="heading-box">Cepat & Mudah</div>
+               <p class="sub-heading-box">
+                   Kami menyediakan berbagai pilihan template untuk <br />undangan digital kamu. <br />Eits, ada
+                   ragam
+                   pilihan music juga loh
+               </p>
+           </div>
+
+       </div>
+       <div class="section-3">
+           <div class="frame-phone">
+               <img class="phone" src="resource/img/images-about-app-1.png" />
+               <div class="circle">
+                   <div class="ellipse-6"></div>
+                   <div class="ellipse-7"></div>
+               </div>
+           </div>
+           <div class="frame-section-3">
+               <div class="heading-section-3">Trend&nbsp;&nbsp;Undangan</div>
+               <p class="sub-heading-section-3">
+                   Di masa sekarang, undangan digital sudah menjadi kebutuhan dalam setiap event. Kami bertekad untuk
+                   membantu kamu dalam memberikan <br />kemudahan dan pelayanan yang maksimal agar kamu bisa
+                   menciptakan
+                   undangan digital kamu sendiri dengan mudah dan cepat.
+               </p>
+               <div class="button-section-3">
+                   <a href="#" class="text-button-section-3">Buat Undangan</a>
+               </div>
+           </div>
+       </div>
+       <div class="section-4">
+           <div class="frame-heading-section-4">
+               <div class="heading-section-4">Paket Website</div>
+               <p class="sub-heading-section-4">Pilih paket layanan website undangan pernikahan sesuai kebutuhan kamu.
+               </p>
+           </div>
+           <div class="frame-box-section-4">
+               <div class="box-1-section-4">
+                   <div class="heading-box-section-4">Free Package</div>
+                   <div class="sub-heading-price-box">IDR 0</div>
+                   <p class="box-text-section-4">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                       labore
+                       et
+                       dolore magna aliqua. Ut enim ad minim veniam
+                   </p>
+                   <div class="button-section-4">
+                       <a href="#" class="text-button-section-4">Buat Sekarang</a>
+                   </div>
+                   <div class="frame-preview">
+                       <img class="eye-vector" src="resource/img/EyeOutline.svg" />
+                       <div class="preview">Preview</div>
+                   </div>
+               </div>
+               <div class="box-2-section-4">
+                   <div class="heading-box-section-4">Silver Package</div>
+                   <div class="sub-heading-price-box">IDR 49.000</div>
+                   <p class="box-text-section-4">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                       labore
+                       et
+                       dolore magna aliqua. Ut enim ad minim veniam
+                   </p>
+                   <div class="button-section-4">
+                       <a href="#" class="text-button-section-4">Buat Sekarang</a>
+                   </div>
+                   <div class="frame-preview">
+                       <img class="eye-vector" src="resource/img/EyeOutline.svg" />
+                       <div class="preview">Preview</div>
+                   </div>
+               </div>
+               <div class="box-3-section-4">
+                   <div class="heading-box-section-4">Free Package</div>
+                   <div class="sub-heading-price-box">IDR 74.000</div>
+                   <p class="box-text-section-4">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                       labore
+                       et
+                       dolore magna aliqua. Ut enim ad minim veniam
+                   </p>
+                   <div class="button-section-4">
+                       <a href="#" class="text-button-section-4">Buat Sekarang</a>
+                   </div>
+                   <div class="frame-preview">
+                       <img class="eye-vector" src="resource/img/EyeOutline.svg" />
+                       <div class="preview">Preview</div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
---
+++ app/Views/template/footer.php
@@ -0,0 +1,24 @@
+        <footer class="footer">
+            <div class="frame-footer">
+                <div class="frame-whats">
+                    <img class="whatsapp" src="resource/img/whats-app.png" />
+                    <div class="no_telp">0882-9755-6125</div>
+                </div>
+                <div class="frame-ig">
+                    <img class="instagram" src="resource/img/instagram-circle.png" />
+                    <div class="ig">@kans_wedding</div>
+                </div>
+                <div class="frame-mail">
+                    <img class="love-mail" src="resource/img/love-mail.png" />
+                    <div class="mail">kanswedding@email.com</div>
+                </div>
+            </div>
+            <div class="frame-dev">
+                <img class="copyright" src="resource/img/copyright.png" />
+                <div class="tgl">2023-2024 Kans wedding</div>
+                <div class="dev">Developed by Kelompok 2 RPL </div>
+            </div>
+        </footer>
+        </body>
+
+        </html>
